56c2492718737503e28347d172dd1a7f496d84f4
Alex/sprocket <Alexhb61@users.noreply.github.com>
2015-01-12 14:35:29 -0800

    Update game.py
    starting code

--- game.py
+++ game.py
@@ -1,1 +1,13 @@
-
+#This is the base class for all objects that will be displayed
+game_objects = []
+def shape(object):
+  def __init__(self):
+    game_objects.append(self)
+  def update(self):
+  def display(self):
+  enabled = False
+  gamespace = 'null'
+  def gs(self,arg):
+    enabled = (arg == gamespace)
+  def gschange(self,arg):
+    gamespace = arg
