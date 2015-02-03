99173da9bb5c1995e5882c730b78e7c7f0dcf804
Alex/sprocket <Alexhb61@users.noreply.github.com>
2015-02-02 16:27:38 -0800

    Update game.py
    added during club meeting

--- game.py
+++ game.py
@@ -1,13 +1,15 @@
 #This is the base class for all objects that will be displayed
 game_objects = []
 def shape(object):
+  #this function adds it to the list of objects to be updated.
   def __init__(self):
     game_objects.append(self)
   def update(self):
   def display(self):
   enabled = False
-  gamespace = 'null'
-  def gs(self,arg):
-    enabled = (arg == gamespace)
-  def gschange(self,arg):
-    gamespace = arg
+  def enabling(self,arg):
+    enabled = arg
+    
+def circle(shape):
+  
+
