5007ec3411930bc6ea1a2f185e3dd46007c2be90
Alex/sprocket <Alexhb61@users.noreply.github.com>
2015-03-12 06:32:11 -0700

    Ready for monday

--- ball.py
+++ ball.py
@@ -1,1 +1,6 @@
+#Note I will fix the dependencies-alex, do not worry about it.
+import game.py
 
+def ball(circle):
+  #add a new init function that asks for a velocity, and a direction(only four options) and runs the old function
+  #add a new update function that changes its position
