99e04eb075d7c6ce87f88e80f9df9f91b344ba22
Alex/sprocket <Alexhb61@users.noreply.github.com>
2015-03-28 02:20:16 -0700

    Ready for Monday
    These are not easy level assignments

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import game.py  
+#do not worry about dependencies, I will handle them - alex
+
+def AI():# Assignment 9 this function should take in an object of the class ball, and return a lambda function, that produces a velocity
+
+def Control():# Assignment 10 this function should take in certain "key" and then using the pygame engine return a lambda function that produces a velocity based on keypress
 
