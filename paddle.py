20bdf018dc741eec62024c4335c58e7831e3001b
Alex/sprocket <Alexhb61@users.noreply.github.com>
2015-03-28 02:22:55 -0700

    Ready for monday
    I had incorrectly used the syntax for classes.

--- paddle.py
+++ paddle.py
@@ -1,7 +1,7 @@
 import game.py
 #Do not worry about the dependencies, I will deal with them.
 
-def paddle(rectangle):
+class paddle(rectangle):
   #assignment 7 run the old init function, and get and store whether or not it is player controlled, and the function that returns the current velocity, and name it currentV
     #note-> look into how laymbda behaves.
   #assignment 8 make a new method for update that changes the position based on the current velocity.
