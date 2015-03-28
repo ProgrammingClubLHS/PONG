62af94d899b4130aa68b9bebc558c0e2b735f9f6
Alex/sprocket <Alexhb61@users.noreply.github.com>
2015-03-28 02:22:08 -0700

    Ready for monday
    I realized i had incorrectly done the syntax for classes

--- ball.py
+++ ball.py
@@ -1,6 +1,6 @@
 #Note I will fix the dependencies-alex, do not worry about it.
 import game.py
 
-def ball(circle):
+class ball(circle):
   #Assignment #5 add a new init function that asks for a velocity, and a direction(only four options) and runs the old function
   #Assignment #6 add a new update function that changes its position and then runs the old update
